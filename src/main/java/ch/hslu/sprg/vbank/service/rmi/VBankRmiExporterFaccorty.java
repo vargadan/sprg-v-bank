8b0ace05030e2f6ab2412bc1418a7786ef8bce22
Daniel Varga <danielvrg@icloud.com>
2019-12-16 02:02:46 +0100

    serialization

---
+++ src/main/java/ch/hslu/sprg/vbank/service/rmi/VBankRmiExporterFaccorty.java
@@ -0,0 +1,30 @@
+package ch.hslu.sprg.vbank.service.rmi;
+
+import ch.hslu.sprg.vbank.service.AccountService;
+import ch.hslu.sprg.vbank.service.impl.JDBCAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.rmi.RmiServiceExporter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VBankRmiExporterFaccorty  {
+
+    public static final int RMI_PORT = 1099;
+
+    @Autowired
+    JDBCAccountService implementation;
+
+    @Bean
+    RmiServiceExporter exporter() {
+        Class<AccountService> serviceInterface = AccountService.class;
+        RmiServiceExporter exporter = new RmiServiceExporter();
+        exporter.setServiceInterface(serviceInterface);
+        exporter.setService(implementation);
+        exporter.setServiceName("X");
+        exporter.setRegistryPort(RMI_PORT);
+        return exporter;
+    }
+
+}
